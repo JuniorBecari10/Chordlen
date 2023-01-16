f4faecd97271cf32f8e46e5b5344b0a3bf1692e3
Antônio Carlos <juniorbecari@gmail.com>
2023-01-16 09:07:22 -0300

    Added transition

--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,7 @@ const modeBtn = qs("#mode-btn");
 const tabOpts = qsAll("footer .opt");
 const tabConts = qsAll("main .tab-cnt");
 
-const emptyMoon = qs("#empty-moon");
+//const emptyMoon = qs("#empty-moon");
 const fullMoon = qs("#full-moon");
 
 var mode = false;
@@ -30,16 +30,10 @@ function updateMode(mode) {
   if (mode) {
     r.style.setProperty("--bg", "#333");
     r.style.setProperty("--fg", "#fff");
-    
-    emptyMoon.classList.add("disabled");
-    fullMoon.classList.remove("disabled");
   }
   else {
     r.style.setProperty("--bg", "#fff");
     r.style.setProperty("--fg", "#000");
-    
-    emptyMoon.classList.remove("disabled");
-    fullMoon.classList.add("disabled");
   }
   
   localStorage["mode"] = mode;
